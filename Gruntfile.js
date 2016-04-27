d3864dcdda6c92f1aeff97bfbcdd852b730fefa3
Oopsno <yufeiliu94@gmail.com>
2016-04-28 00:20:56 +0800

    add grunt-babel to support es6`

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ const harmonyFlags = ['--harmony_destructuring', '--harmony_default_parameters']
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-mocha-istanbul');
+  grunt.loadNpmTasks('grunt-babel');
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     mochaTest: {
@@ -18,6 +19,18 @@ module.exports = function (grunt) {
         options: {
           mochaOptions: harmonyFlags,
           istanbulOptions: harmonyFlags
+        }
+      }
+    },
+    babel: {
+      dist: {
+        options: {
+          presets: ['es2015']
+        },
+        files: {
+          'dist/lib/API.js': 'lib/API.js',
+          'dist/lib/config.js': 'lib/config.js',
+          'dist/lib/assistant.js': 'lib/assistant.js'
         }
       }
     }
